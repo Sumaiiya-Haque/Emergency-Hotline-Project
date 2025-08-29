1de135206c94e330b60b440cf2520ab3bf654bea
Sumaiiya-Haque <sumaiyahaque475@gmail.com>
2025-08-29 16:29:13 +0600

    last commit

--- index.js
+++ index.js
@@ -303,5 +303,17 @@ document.getElementById("clear-history-btn").addEventListener("click", function(
     })  
   });
     
+// copy count
 
+let copyCount = document.getElementById("copy-count");
+let count3 = parseInt(copyCount.innerText) || 0;
+
+let copyIcons = document.querySelectorAll(".copy-card-btn");
+
+for (let i = 0; i < copyIcons.length; i++) {
+  copyIcons[i].onclick = function() {
+    count3++;
+    copyCount.innerText = count3;
+  }
+}
 
